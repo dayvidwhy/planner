7b0943727c267104031dd5902452931d5dbe617d
dayvidwhy <dayvidwhy@gmail.com>
2024-03-16 10:35:13 +1000

    Added navigation.

--- app/components/navigation.tsx
+++ app/components/navigation.tsx
@@ -15,28 +15,42 @@ export default function Navigation() {
   return (
     <NavigationMenu>
       <NavigationMenuList>
-        <NavigationMenuItem>
-            <Link href="/about" legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                    About
-                </NavigationMenuLink>
-            </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-            <Link href="/docs" legacyBehavior passHref>
-                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                    Contact
-                </NavigationMenuLink>
-            </Link>
-        </NavigationMenuItem>
-        <NavigationMenuItem>
-          <Link href="/docs" legacyBehavior passHref>
-            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                Documentation
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-      </NavigationMenuList>
+            <NavigationMenuItem>
+                <Link href="/" legacyBehavior passHref>
+                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                        Home
+                    </NavigationMenuLink>
+                </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+                <Link href="/about" legacyBehavior passHref>
+                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                        About
+                    </NavigationMenuLink>
+                </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+                <Link href="/contact" legacyBehavior passHref>
+                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                        Contact
+                    </NavigationMenuLink>
+                </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+                <Link href="/login" legacyBehavior passHref>
+                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                        Login
+                    </NavigationMenuLink>
+                </Link>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+                <Link href="/signup" legacyBehavior passHref>
+                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                        Sign Up
+                    </NavigationMenuLink>
+                </Link>
+            </NavigationMenuItem>
+        </NavigationMenuList>
     </NavigationMenu>
   )
 };
---
+++ app/signup/page.tsx
@@ -0,0 +1,8 @@
+export default function Signup() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        Sign Up
+      </main>
+    );
+  };
+  

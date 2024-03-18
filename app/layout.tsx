fc03b6357ffb9041a103d4eca6875edadd96b0fa
dayvidwhy <dayvidwhy@gmail.com>
2024-03-18 18:54:57 +1000

    Sign out with redirect.

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default async function RootLayout({
                             }}
                             signOut={async () => {
                                 "use server";
-                                await signOut();
+                                await signOut({redirectTo: "/"});
                             }}
                             loggedIn={session !== null}
                         />
